57efd2725cc76373d0165627788075d0b484496c
Tracy-miranja <barazatracy44@gmail.com>
2023-06-16 08:34:45 +0300

    Implement required changes

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,10 @@ class Post < ApplicationRecord
 
   after_create :update_user_posts_counter
 
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+
   private
 
   def update_user_posts_counter
