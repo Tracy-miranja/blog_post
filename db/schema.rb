865a1db2a08a9ca939d40050531a1bd0268d0e41
Tracy-miranja <barazatracy44@gmail.com>
2023-06-16 23:39:14 +0300

    Add test for user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_14_205356) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_16_195734) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,19 +18,23 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_14_205356) do
     t.string "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "author_id", null: false
+    t.bigint "user_id", null: false
     t.bigint "post_id", null: false
+    t.bigint "author_id"
     t.index ["author_id"], name: "index_comments_on_author_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "likes", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "author_id", null: false
+    t.bigint "user_id", null: false
+    t.string "address"
     t.bigint "post_id", null: false
-    t.index ["author_id"], name: "index_likes_on_author_id"
+    t.index ["address"], name: "index_likes_on_address"
     t.index ["post_id"], name: "index_likes_on_post_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -38,10 +42,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_14_205356) do
     t.string "text"
     t.integer "comments_counter", default: 0
     t.integer "likes_counter", default: 0
-    t.bigint "author_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.string "address"
+    t.bigint "author_id"
+    t.index ["address"], name: "index_posts_on_address"
     t.index ["author_id"], name: "index_posts_on_author_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -51,11 +59,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_14_205356) do
     t.integer "post_counter", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "posts_counter", default: 0
   end
 
   add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "comments", "users", column: "author_id"
   add_foreign_key "likes", "posts"
-  add_foreign_key "likes", "users", column: "author_id"
+  add_foreign_key "likes", "users"
+  add_foreign_key "posts", "users"
   add_foreign_key "posts", "users", column: "author_id"
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { User.new(name: "kushie tracy", posts_counter: 5) }
+
+  before { subject.save }
+
+  it "is valid with valid attributes" do
+    expect(subject).to be_valid
+  end
+
+  it "is not valid without a name" do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid with posts_counter less than zero" do
+    subject.posts_counter = -1
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid with non-integer posts_counter" do
+    subject.posts_counter = "hello"
+    expect(subject).to_not be_valid
+  end
+
+  # describe "#recent_posts" do
+  #   let!(:user) { User.create(name: "kushie tracy", posts_counter: 5) }
+  #   let!(:post1) { Post.create(title: "Post 1", author: user, created_at: 1.hour.ago) }
+  #   let!(:post2) { Post.create(title: "Post 2", author: user, created_at: 2.hours.ago) }
+  #   let!(:post3) { Post.create(title: "Post 3", author: user, created_at: 3.hours.ago) }
+  #   let!(:post4) { Post.create(title: "Post 4", author: user, created_at: 4.hours.ago) }
+
+  #   it "returns the most recent 3 posts" do
+  #     expect(user.recent_posts).to eq([post4, post3, post2])
+  #   end
+  # end
 end
