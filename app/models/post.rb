f8246bcc3d7b760c4ebef963bb5c4db10b331da1
Tracy-miranja <barazatracy44@gmail.com>
2023-06-16 11:45:24 +0300

    Add validation for post

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,10 @@ class Post < ApplicationRecord
   has_many :likes
   belongs_to :author, class_name: 'User', foreign_key: 'user_id'
 
+  validates :title presence:true, length: {in: 1...250}
+  validates :comments_counter presence:true, numericality: {greater_than_or_equal_to: 0, only_integer: true}
+  validates :likes_counter presence:true, numericality: {greater_than_or_equal_to: 0, only_integer: true}
+
   after_create :update_user_posts_counter
 
   def recent_comments
