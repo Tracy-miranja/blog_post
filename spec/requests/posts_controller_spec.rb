f4e5a0f1ea0ef258441215ff266ec6bf10c03989
Tracy-miranja <barazatracy44@gmail.com>
2023-06-21 14:42:58 +0300

    Correct linter

--- spec/requests/posts_controller_spec.rb
+++ spec/requests/posts_controller_spec.rb
@@ -1,21 +1,20 @@
 RSpec.describe 'Controllers', type: :request do
-    describe 'PostsController' do
-      describe 'GET #index' do
-        it 'returns a successful response' do
-          get posts_path
-          expect(response).to be_successful
-        end
-  
-        it 'renders the index template' do
-          get posts_path
-          expect(response).to render_template(:index)
-        end
-  
-        it 'includes correct placeholder text in the response body' do
-          get posts_path
-          expect(response.body).to include('Display posts')
-        end
+  describe 'PostsController' do
+    describe 'GET #index' do
+      it 'returns a successful response' do
+        get posts_path
+        expect(response).to be_successful
+      end
+
+      it 'renders the index template' do
+        get posts_path
+        expect(response).to render_template(:index)
+      end
+
+      it 'includes correct placeholder text in the response body' do
+        get posts_path
+        expect(response.body).to include('Display posts')
       end
     end
   end
-  
+end
--- spec/requests/users_controller_spec.rb
+++ spec/requests/users_controller_spec.rb
@@ -1,12 +1,11 @@
 require 'rails_helper'
 RSpec.describe UsersController, type: :controller do
-    describe 'GET index' do
-      it 'assigns @users' do
-        user = User.create(name: 'John Doe')
-        get :index
-        expect(response).to be_successful
-        expect(assigns(:users)).to eq([user])
-      end
+  describe 'GET index' do
+    it 'assigns @users' do
+      user = User.create(name: 'John Doe')
+      get :index
+      expect(response).to be_successful
+      expect(assigns(:users)).to eq([user])
     end
   end
-  
+end
