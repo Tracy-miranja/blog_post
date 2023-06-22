8a5721006d547a4d1973eadaefa31e6d8a3027ca
Tracy-miranja <barazatracy44@gmail.com>
2023-06-22 23:17:00 +0300

    Add view to my project

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,13 @@
 class PostsController < ApplicationController
+ layout 'standard'
   def index
     @posts = Post.all
+    @users= User.all
   end
 
   def show
     @post = Post.find(params[:id])
+    @user = User.find(params[:user_id])
+    @comments = Comment.where(post_id: params[:id])
   end
 end
