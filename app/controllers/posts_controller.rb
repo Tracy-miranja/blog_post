d0e3f7c0b4f9942c0dadbcd5df5477f8d86ebdaa
Tracy-miranja <barazatracy44@gmail.com>
2023-06-23 00:15:13 +0300

    Add info on the show files

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,12 @@
 class PostsController < ApplicationController
  layout 'standard'
   def index
-    @posts = Post.all
-    @users= User.all
+    @user = User.find(params[:user_id])
+    @posts = @user.posts
   end
 
   def show
     @post = Post.find(params[:id])
-    @user = User.find(params[:user_id])
-    @comments = Comment.where(post_id: params[:id])
+    @comments = @post.comments
   end
 end
