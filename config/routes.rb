d792817a2f7206d4e639414ac322b7c6f39f8d14
Tracy-miranja <barazatracy44@gmail.com>
2023-06-23 08:35:11 +0300

    Add link to the users path

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get 'users/:user_id/posts', to: 'posts#index', as: 'user_posts'
   get 'users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
   get 'posts', to: 'posts#index'
+  resources :users
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
