95867fd822ae4a8ad6206b538c78bdbf4489b693
Tracy-miranja <barazatracy44@gmail.com>
2023-06-27 12:03:34 +0300

    Add user index test

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe "Users", type: :system do
   end
   
   scenario 'redirects to user show page when clicking on a user' do 
-    user = User.create(name: 'Lilly', posts_counter: 2, photo: 'https://randomuser.me/api/portraits/women/70.jpg',
+    user = User.create(name: 'Lilly', posts_counter: 4, photo: 'https://randomuser.me/api/portraits/women/70.jpg',
                        bio: 'Teacher from Poland.')
   
     visit users_path(user)
@@ -28,6 +28,28 @@ RSpec.describe "Users", type: :system do
     end
   
     expect(page).to have_content('Lilly')
+  end
+
+  describe 'User show page' do 
+    scenario 'displays the user profile picture, username, number of posts, bio, and first 3 posts' do
+      user = User.create(name: 'Lilly', posts_counter: 3,  photo: 'https://randomuser.me/api/portraits/women/70.jpg', 
+                         bio: 'Teacher from Poland.' )
+         visit user_path(user)
+         expect(page).to have_content('Lilly')  
+         expect(page).to have_content(3)
+         expect(page).to have_content('Teacher from Poland.')             
+    end
+
+    scenario 'redirects to post show page when clicking on a user post' do 
+      user = User.create(name: 'Lilly', posts_counter: 4, photo: 'https://randomuser.me/api/portraits/women/70.jpg',
+                       bio: 'Teacher from Poland.')
+      post = Post.create(title: 'new post', text: 'hello world', author: user)
+
+      visit user_path(user)
+      click_link 'See All Posts'
+      expect(page).to have_content('hello world')
+      
+    end
   end
   
 end
