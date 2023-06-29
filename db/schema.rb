e3fb02db213fb4172881de4ddc2e92a2a1844f9a
Tracy-miranja <barazatracy44@gmail.com>
2023-06-29 15:32:37 +0300

    Add devise to my repo

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_17_192528) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_28_152617) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -51,6 +51,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_17_192528) do
     t.integer "post_counter", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "user_name"
+    t.string "password"
+    t.string "Log_in"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.integer "posts_counter", default: 0
   end
 
